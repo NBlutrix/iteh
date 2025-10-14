9139f5b97f84e916bec2d7a806e1c23d7839a09e
Nikola <nikolamandan@gmail.com>
2025-10-14 21:51:31 +0200

    Create movie tabela

--- backend/database/migrations/2025_10_14_194131_create_movies_table.php
+++ backend/database/migrations/2025_10_14_194131_create_movies_table.php
@@ -4,28 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->integer('release_year')->nullable();
+            $table->string('poster_path')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(): void {
         Schema::dropIfExists('movies');
     }
 };
+
