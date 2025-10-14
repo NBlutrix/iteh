715344e82b048418c04b7824f21c032436cbdde7
Nikola <nikolamandan@gmail.com>
2025-10-14 21:50:56 +0200

    Create genres tabela

--- backend/database/migrations/2025_10_14_194126_create_genres_table.php
+++ backend/database/migrations/2025_10_14_194126_create_genres_table.php
@@ -4,28 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(): void {
         Schema::dropIfExists('genres');
     }
 };
+
